4925c810cb6ecbe58b65d80717431095cd646437
tschoerv <tschoerv@protonmail.com>
2024-03-28 14:45:26 +0100

    fix img not showing

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
+    images: { unoptimized: true },
     webpack: config => {
       config.resolve.fallback = { fs: false, net: false, tls: false };
       config.externals.push("pino-pretty", "lokijs", "encoding");
