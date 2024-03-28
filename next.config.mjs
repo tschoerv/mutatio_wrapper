53216157828e9222ddc6354f9f23e7f97d6bc10e
tschoerv <tschoerv@protonmail.com>
2024-03-28 14:31:40 +0100

    fleek deployment adjustments

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
     webpack: config => {
       config.resolve.fallback = { fs: false, net: false, tls: false };
       config.externals.push("pino-pretty", "lokijs", "encoding");
