32378e66187efc1d0bbe2f21889dd91762472b10
tschoerv <tschoerv@protonmail.com>
2026-01-05 15:37:49 +0100

    lint update 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
-  ...compat.extends("next/core-web-vitals"),
+  ...nextCoreWebVitals,
 
   // Your ignores / overrides go here
   {
